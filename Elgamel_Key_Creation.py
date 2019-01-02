cc74eeba067b82624af6f16edc59940cfde7207c
Bradley Soo <38500505+Themathematician314@users.noreply.github.com>
2019-01-01 19:28:00 -0500

    Update Elgamel_Key_Creation.py

--- Elgamel_Key_Creation.py
+++ Elgamel_Key_Creation.py
@@ -1,2 +1,26 @@
 import math
-import string
+import random
+import sys
+import os
+
+class Public_Key(object):
+    
+    def __init__(self, prime=None, g=None, x=None, confidence=None):
+        self.__prime = prime
+        self.__g = g
+        self.__x = x
+        self.__confidence = confidence
+        
+class Private_Key(object):
+    
+    def __init__(self, prime=None, g=None, x=None, confidence=None):
+        self.__prime = prime
+        self.__g = g
+        self.__x = x
+        self.__confidence = confidence
+        
+def gcd(a, b):
+    return math.gcd(a, b)
+
+def power(base, exponent, modulo):
+    return ((base**exponent) % modulo)
